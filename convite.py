acef4e95dc3910c14a00f72dd32fc54a1e70754c
Bernardo <bernardo.araujo1612@gmail.com>
2024-12-21 16:00:03 -0300

    ajuste de menu

--- convite.py
+++ convite.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.set_page_config(
     page_title="Convite Luísa",
@@ -6,16 +7,29 @@ st.set_page_config(
     )
 
 
-st.title("Aniversário da Luísa")
+# st.title("Aniversário da Luísa")
 
-invite, local, suggestion = st.tabs(["Convite", "Localização", "Sugestão de Presente"])
-
-
-with invite:
-    st.write("CONVITE")
+# invite, local, suggestion = st.tabs(["Convite", "Localização", "Sugestão de Presente"])
+# with invite:
+#     st.write("CONVITE")
     
-with local:
-    st.write("LOCAL")
+# with local:
+#     st.write("LOCAL")
     
-with suggestion:
-    st.write("SUGESTÃO DE PRESENTE")
+# with suggestion:
+#     st.write("SUGESTÃO DE PRESENTE")\
+
+
+
+with st.container():
+    selected = option_menu(
+        menu_title= "CONTAMOS COM SUA PRESENÇA!",
+        options=["Convite", "Local", "Sugestão de Presente"],
+        icons=["envelope", "house", "gift"],    
+        menu_icon="star",
+        default_index=0,
+        orientation="horizontal",  
+    )
+
+st.write(f"Você selecionou: {selected}, Bem-vind")
+
